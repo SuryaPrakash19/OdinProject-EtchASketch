6326c98f16a51a73f46d0ac0f30042b799115a27
Surya Prakash S <suryas190299@gmail.com>
2024-07-20 14:38:26 +0530

    Opacity added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const c = document.getElementById("container");
 let boxes;
-makeGrid(16);
+makeGrid(70);
 function makeGrid(inp) {
   while (c.firstChild) {
     c.removeChild(c.firstChild);
@@ -9,9 +9,11 @@ function makeGrid(inp) {
     const b = document.createElement("div");
     const l = 800 / inp;
     const s = l + "px";
-
+    b.style.opacity = 0;
     b.style.setProperty("width", s);
     b.style.setProperty("height", s);
+    b.style.backgroundColor = "red";
+    b.style.border = "1px solid red";
     b.className = "box";
 
     c.appendChild(b);
@@ -23,16 +25,23 @@ function events() {
   boxes = document.getElementsByClassName("box");
   for (let box of boxes) {
     box.addEventListener("mouseover", () => {
-      box.style.backgroundColor = "red";
+      let x = parseFloat(box.style.opacity) + 0.2;
+      box.style.setProperty("opacity", x);
     });
   }
 }
 function clearFn() {
+  boxes = document.getElementsByClassName("box");
   for (let box of boxes) {
-    box.style.backgroundColor = "white";
+    box.style.opacity = 0;
   }
 }
 function inputGrid() {
   const inp = window.prompt("Enter grid dimension");
-  makeGrid(inp);
+  const i = parseInt(inp);
+  if (!isNaN(i) && i > 0 && i <= 100) {
+    makeGrid(i);
+  } else {
+    window.alert("Enter a number less than 100!");
+  }
 }
